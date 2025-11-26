4e94bcb9b96abb6f3e6cfec5476069a9a6467b6e
Gyula <erdeigyula3@gmail.com>
2025-11-26 12:39:59 +0100

    egyesites függvény implementálása

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function halmazBeolvas(input) {
+function halmazBeolvas(bemenetek) {
     let halmaz = new Set();
     for (let i = 0; i < arguments.length; i++) {
         halmaz.add(arguments[i]);
     }
     return halmaz;
+}
+
+function egyesites(halmaz1, halmaz2) {
+    for (let elem of halmaz2) {
+        halmaz1.add(elem);
+    }
+    return halmaz1;
 }
